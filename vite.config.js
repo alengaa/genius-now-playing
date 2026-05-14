2883ae890cfbd30909430b96e38c1a82d05d26af
AlengaISC <alenga@AlengaPC.localdomain>
2026-05-14 21:17:26 +0200

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// vite.config.js
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+	plugins: [vue()],
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+				background: resolve(__dirname, 'src/background/index.js'),
+				content: resolve(__dirname, 'src/content/index.js'),
+			},
+			output: {
+				entryFileNames: 'assets/[name].js',
+			},
+		},
+	},
+});
